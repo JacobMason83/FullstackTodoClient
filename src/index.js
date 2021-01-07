bad5354abef89218953adf8e95d517cbf8226051
JacobMason83 <jmason062083@gmail.com>
2021-01-07 12:57:57 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,71 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import './styles.css'
+import axios from 'axios'
+
+
+
+class App extends Component {
+  constructor(){
+    super()
+    
+    this.state = {
+      todo: "",
+      todos: []
+    }
+  }
+  componentDidMount() {
+    axios
+    .get('http://jdm-todo-api.herokuapp.com/todos')
+    .then(res => this.setState({
+      todos: res.data
+    }))  
+    .catch(err => console.error(err))
+  }
+  handleChange = e => {
+    this.setState({
+      todo: e.target.value
+    })
+  }
+  handleSubmit = e => {
+    e.preventDefault()
+    console.log('submitted')
+  }
+  renderTodos = () => {
+    return this.state.todos.map(todo => {
+      return(
+        <div>
+          <h1>{todo.title}</h1>
+        </div>
+      )
+    })
+  }
+  
+  render(){
+  return(
+    <div id='root'>
+      <h1>ToDo List</h1>
+      <form className='add-todo' onSubmit={this.handleSubmit}>
+        <input type="text"
+          placeholder="Add ToDo"
+          onChange={this.handleChange}
+          value={this.state.todo}
+        />
+        <button type="submit">Add</button>
+      </form>
+      {this.renderTodos()}
+    </div>
+  )
+}
+}
+
+ReactDOM.render(  
+    <App />, document.getElementById('root')
+)
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
